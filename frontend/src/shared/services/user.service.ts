6afd6ef849d94ef83cc0cb6ba8a4db4d3892e5da
Laurent Tetier <laurent.tetier@gmail.com>
2024-05-06 11:31:28 +0200

    fix linter

--- frontend/src/shared/services/user.service.ts
+++ frontend/src/shared/services/user.service.ts
@@ -4,21 +4,17 @@ import type { User, UserForm } from "../interfaces";
 const BASE_URL = '/api/user';
 
 export async function createUser(userForm: UserForm) {
-    try {
-        const response = await fetch(BASE_URL, {
-            method: 'POST',
-            body: JSON.stringify(userForm),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw await response.json();
+    const response = await fetch(BASE_URL, {
+        method: 'POST',
+        body: JSON.stringify(userForm),
+        headers: {
+            'Content-Type': 'application/json'
         }
-    } catch (e) {
-        throw e;
+    });
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw await response.json();
     }
 }
 
